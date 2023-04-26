8396a38c5a2aa14e9ca8e667bdbf846a4a681084
tonantr <tonantruong@hotmail.com>
2023-04-26 10:41:37 -0400

    complete Building the Simple Liker App

--- main.js
+++ main.js
@@ -3,7 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+function failureStatus(err) {
+  const hiddenClass = document.querySelector('.hidden');
+  hiddenClass.style.visibility = 'visible';
+  const p = document.querySelector('#modal-message');
+  p.innerText = err;
+  setTimeout(() => {
+    hiddenClass.style.visibility = 'hidden';
+  }, 3000);
+};
 
+
+
+function liking() {
+  const items = document.querySelectorAll('.like-glyph');
+  items.forEach(element => {
+    element.addEventListener('click', () => {
+      const myPromise = mimicServerCall();
+      myPromise
+        .then(() => {
+          if (element.innerText === EMPTY_HEART) {
+            element.innerText = '♥';
+          } else if (element.innerText === FULL_HEART) {
+            element.innerText = '♡';
+          }
+        })
+        .catch(err => failureStatus(err))
+    });
+  })
+};
+
+liking()
 
 
 
@@ -11,9 +41,9 @@ const FULL_HEART = '♥'
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
-  return new Promise(function(resolve, reject) {
-    setTimeout(function() {
+function mimicServerCall(url = "http://mimicServer.example.com", config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
       let isRandomFailure = Math.random() < .2
       if (isRandomFailure) {
         reject("Random server error. Try again.");
